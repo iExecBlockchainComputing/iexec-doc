19022e131d7bcbc8e5e9595ed9813acadbc27537
eric rodriguez <er@iex.ec>
2019-02-05 16:38:31 +0100

    remove a double word

--- conf.py
+++ conf.py
@@ -169,7 +169,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'iExecProjectDocumentation.tex', u'iExec Project Documentation Documentation',
+    (master_doc, 'iExecProjectDocumentation.tex', u'iExec Project Documentation',
      u'iExec Team', 'manual'),
 ]
 
@@ -179,7 +179,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'iexecprojectdocumentation', u'iExec Project Documentation Documentation',
+    (master_doc, 'iexecprojectdocumentation', u'iExec Project Documentation',
      [author], 1)
 ]
 
